862322b30d61785059bd85f19d647beea497af21
aleeeexcool <ramzhev.sasha@outlook.com>
2023-04-29 21:20:19 +0300

    deploy script update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,24 +1,63 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
+const ethers = hre.ethers;
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  if (network.name === "hardhat") {
+    console.warn(
+      "You are trying to deploy a contract to the Hardhat Network, which" +
+      "gets automatically created and destroyed every time. Use the Hardhat" +
+      " option '--network localhost'"
+    );
+  }
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  const [deployer] = await ethers.getSigners()
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log("Deploying with", await deployer.getAddress())
 
-  await lock.deployed();
+  const DutchAuction = await ethers.getContractFactory("DutchAuction", deployer)
+  const auction = await DutchAuction.deploy(
+    ethers.utils.parseEther('2.0'),
+    1,
+    "New NFT"
+  )
+  await auction.deployed()
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  saveFrontendFiles({
+    DutchAuction: auction
+  })
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+function saveFrontendFiles(contracts) {
+  const contractsDir = path.join(__dirname, '/..', 'front/contracts')
+
+  if(!fs.existsSync(contractsDir)) {
+    fs.mkdirSync(contractsDir)
+  }
+
+  Object.entries(contracts).forEach((contract_item) => {
+    const [name, contract] = contract_item
+
+    if(contract) {
+      fs.writeFileSync(
+        path.join(contractsDir, '/', name + '-contract-address.json'),
+        JSON.stringify({[name]: contract.address}, undefined, 2)
+      )
+    }
+
+    const ContractArtifact = hre.artifacts.readArtifactSync(name)
+
+    fs.writeFileSync(
+      path.join(contractsDir, '/', name + ".json"),
+      JSON.stringify(ContractArtifact, null, 2)
+    )
+  })
+}
+
+ main()
+   .then(() => process.exit(0))
+   .catch((error) => {
+     console.error(error)
+     process.exit(1)
+   })
